91e3d7a5d01a2d6d1a75179fe544b56c151c43cc
arinsoni <arinsoni.iitb@gmail.com>
2023-06-03 14:48:15 +0530

    NavBar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from '../src/pages/About';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       
+       <BrowserRouter>
+       <NavBar/>
+        <Routes>
+          <Route exact path='/home' element={ <Home/> } />
+          <Route exact path='/about' element={ <About/> } />
+        </Routes>
+       </BrowserRouter>
+    </>
+  
+
   );
 }
 
