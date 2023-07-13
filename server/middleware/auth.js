7343b0f4e4cdbc8aa16e1ae79569f42f59f1a5f0
arinsoni <arinsoni.iitb@gmail.com>
2023-07-13 13:12:33 +0530

    cors added to middlewarr

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,25 @@
 const JWT = require('jsonwebtoken');
+const cors = require('cors');
 
 const verifyToken = async (req, res, next) => {
-    const token = await req.header('auth-token');
-    if(!token){
-        return res.status(401).json({ error: "Please authenticate using valid token" });
-    }
-    try {
-        const data = JWT.verify(token, process.env.JWT_SECRET);
-        req.user = data.user;
-        next();
-    } catch (error) {
-        res.status(401).json({ error: "Please authenticate using valid token" });
-    }
-}
+  const token = await req.header('auth-token');
+  if (!token) {
+    return res.status(401).json({ error: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = JWT.verify(token, process.env.JWT_SECRET);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Please authenticate using a valid token" });
+  }
+};
 
-module.exports = verifyToken;
+// Enable CORS for all routes
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+const corsMiddleware = cors(corsOptions);
+
+module.exports = [corsMiddleware, verifyToken];
